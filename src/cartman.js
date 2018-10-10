2aa15371a1767433e8af10c469019be66b71607e
Adi Sivasankaran <adi@Adis-MacBook-Pro.local>
2018-10-10 02:21:10 -0500

    Code cleanup and commentary

--- src/cartman.js
+++ src/cartman.js
@@ -8,28 +8,48 @@ const Promise = require('bluebird');
 class Cartman {
   /**
    * @function download
-   * @param {*} url
-   * @param {*} destination
-   * @param {*} portion
-   * @param {*} chunkSize
+   * @description Downloads resources in chunks. Only downloads specified portion of a file
+   * @param {String} url - URL of the resource to download
+   * @param {String} destination - Path to file to which downloaded resource is written (should include file name)
+   * @param {Number} portion - Portion of file in MiB
+   * @param {Number} chunkSize - Size of the chunks in MiB
    */
   static async download(url, destination, portion, chunkSize) {
+    if (chunkSize > portion) {
+      throw new Error('chunk size cannot be bigger than portion size');
+    }
+
     const { MAX_PARALLEL_DOWNLOADS, MEBI_BYTE } = this.getConfig();
 
+    // Ping the resource to get the total size of the file:
     const sizeOfFileInBytes = parseInt(await this.getFileSize(url));
 
+    if (portion * MEBI_BYTE > sizeOfFileInBytes) {
+      throw new Error('Portion size cannot be larger than the file itself');
+    }
+
+    // If no destination provided, set the default destination
+    if(!destination) {
+      destination = './cartman-download';
+    }
+
     let portionBytes;
+    // If no portion is provided, download the entire file:
     if (!portion) {
       portionBytes = sizeOfFileInBytes;
     } else {
+      // Otherwise, convert mebibyte value to bytes for later use:
       portionBytes = portion * MEBI_BYTE;
     }
 
     let chunkSizeBytes;
+    // If no chunk size is provided:
     if (!chunkSize) {
+      // Set it to 1 MiB, if portion size is greater than 1 MiB
       if (portionBytes >= MEBI_BYTE) {
         chunkSizeBytes = MEBI_BYTE;
       } else {
+        // Otherwise, set the default chunk size to 1/5th of what the portion size is:
         chunkSizeBytes = portionBytes/5;
       }
     } else {
@@ -40,27 +60,59 @@ class Cartman {
     const destWriteStream = fs.createWriteStream(destination);
 
     let options;
-    async.eachLimit(byteRanges, MAX_PARALLEL_DOWNLOADS, (range, callback) => {
+
+    /**
+     * Async iteration in series due to:
+     * 1. async nature of HTTP GET calls
+     * 2. the need to preserve the order of data
+     * 
+     * @todo
+     * Previously, successful parallel download was achieved, however, order of data was not preserved
+     * How to download in parallel without losing data order?
+     */
+    async.eachSeries(byteRanges, (range, callback) => {
       options = {
         url: url,
         headers: {
           'Range': range
         }
       };
-      request.get(options).pipe(destWriteStream);
-      callback();
+
+      request.get(options, (err, response, body) => {
+        if (err) {
+          throw err;
+        }
+
+        destWriteStream.write(body, (err) => {
+          if (err) {
+            throw err;
+          }
+          callback();
+        });
+      });
     }, (err) => {
       if (err) {
         console.log(err);
       }
+      destWriteStream.end();
       console.log('Finished downloading');
     });
   }
 
   /**
    * @function calculateByteRanges
-   * @param {*} portion
-   * @param {*} chunkSize
+   * @description Calculate the byte ranges for the range header to be used in each HTTP GET call
+   * @param {Number} portion
+   * @param {Number} chunkSize
+   * @return {Array<String>} List of calculated byte ranges
+   * Example:
+   * [
+   *   'bytes=0-101241',
+   *   'bytes=101242-202482',
+   *   .
+   *   .
+   *   .
+   * ]
    */
   static calculateByteRanges(portionBytes, chunkSizeBytes) {
     const totalRanges = Math.floor(portionBytes/chunkSizeBytes);
@@ -68,6 +120,15 @@ class Cartman {
 
     let start = 0, stop = chunkSizeBytes, range;
 
+    /**
+     * Start from 0 bytes and create byte intervals the following way:
+     * Example range of 5:
+     * [0, 1 x chunkSize]
+     * [1 x chunkSize + 1, 2 x chunkSize]
+     * [2 x chunkSize + 1, 3 x chunkSize]
+     * [3 x chunkSize + 1, 4 x chunkSize]
+     * [4 x chunkSize + 1, totalSizeInBytes]
+     */
     for (let i = 0; i < totalRanges; i++) {
       if (portionBytes - stop > 0 && portionBytes - stop < chunkSizeBytes) {
         stop += (portionBytes - stop);
@@ -86,7 +147,9 @@ class Cartman {
 
   /**
    * @function getFileSize
-   * @param {*} url
+   * @description Pings the resource server for information about the resource size
+   * @param {String} url
+   * @return {String} Resource size or content-length
    */
   static getFileSize(url) {
     return new Promise((resolve, reject) => {
@@ -99,19 +162,17 @@ class Cartman {
     });
   }
 
+  /**
+   * @function getConfig
+   * @description Returns the configuration for the downloads
+   * @return {Object} Config
+   */
   static getConfig() {
     return {
       MAX_PARALLEL_DOWNLOADS: 1000,
       MEBI_BYTE: 1024 * 1024
     };
   }
-
-  static directDownload(url, destination) {
-    const destWriteStream = fs.createWriteStream(destination);
-    request.get(url).pipe(destWriteStream);
-  }
 }
 
-Cartman.download("http://norvig.com/big.txt", "./download-file");
-// Cartman.directDownload("http://norvig.com/big.txt", "./download-file");
-// Cartman.download("http://norvig.com/big.txt", "./download-file-partial", 4, 1);
+module.exports = Cartman;
